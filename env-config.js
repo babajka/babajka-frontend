15b3dea31a61e3cae9a609801f86106e8bca5f9d
uladbohdan <vlbohdan@gmail.com>
2017-12-02 01:51:40 +0300

    fix for frontend environment variable delivery

--- env-config.js
+++ env-config.js
@@ -1,3 +1,3 @@
-const { BABAJKA_BACKEND_URL } = process.env.BABAJKA_BACKEND_URL;
+const { BABAJKA_BACKEND_URL = '' } = process.env;
 
 module.exports = { 'process.env.BABAJKA_BACKEND_URL': BABAJKA_BACKEND_URL };
