01f2c25ef3c0be2136f0f6dfbd0cb83452bb5055
uladbohdan <vlbohdan@gmail.com>
2018-04-17 01:44:48 +0300

    proper static handling

--- bin/copy-static.sh
+++ bin/copy-static.sh
@@ -3,4 +3,6 @@
 rm -rf static
 mkdir static
 
-cp -R "node_modules/babajka-markup/dist/static/" "static/"
+# Please, be aware! cp -R implementation is different in Linux and macOS.
+# The way it is written below works equally on both platforms.
+cp -R "node_modules/babajka-markup/dist/static/." "static/"
