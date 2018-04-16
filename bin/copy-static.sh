e582d1c812fd832fc11cd97d4514efa25ca4f57c
uladbohdan <vlbohdan@gmail.com>
2018-04-17 01:14:22 +0300

    uniform static delivery

--- bin/copy-static.sh
+++ bin/copy-static.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 
-DIST_PATH="node_modules/babajka-markup/dist/"
-
 rm -rf static
 mkdir static
 
-for dir in "fonts" "images" "styles";
-    do
-        cp -R ${DIST_PATH}${dir} static/
-    done
+cp -R "node_modules/babajka-markup/dist/static/" "static/"
--- components/common/layout/CoreLayout.js
+++ components/common/layout/CoreLayout.js
@@ -6,14 +6,14 @@ const CoreLayout = ({ title, children }) => (
   <div>
     <Head>
       <title>
-        Wir {title && '| '}
+        Wir.by {title && '| '}
         {title}
       </title>
       <meta charSet="utf-8" />
       <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <meta name="theme-color" content="#1a9582" />
-      <link rel="icon" type="image/png" href="/static-prod/favicon-colored.png" />
+      <link rel="icon" type="image/png" href="/static/images/logo/favicon-colored.png" />
       <link rel="stylesheet" href="/static/styles/assets.min.css" />
       <link rel="stylesheet" href="/static/styles/bundle.min.css" />
     </Head>
