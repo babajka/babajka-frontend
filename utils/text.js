aec1c99cda75202f9fe6f73d55177ab8e2007155
tyndria <tyndria23@gmail.com>
2018-05-06 21:44:20 +0300

    Remove useless code

--- utils/text.js
+++
@@ -1,6 +0,0 @@
-const getTruncatedText = (text, maxLength) => {
-  const words = text.split(' ');
-  return `${words.slice(0, maxLength).join(' ')}...`;
-};
-
-export { getTruncatedText as default };
