81446d52dbe400100d5644acd5fd2ec1c4910d3f
Ivan Pazhitnykh <ivan.pazhitnykh@gmail.com>
2018-08-05 15:02:55 +0300

    chore(core): add user populating on error page

--- pages/_error.js
+++ pages/_error.js
@@ -4,11 +4,15 @@ import withRedux from 'next-redux-wrapper';
 import PageLayout from 'components/common/layout/PageLayout';
 import { Error404, Error500 } from 'components/common/layout/error';
 
+import { actions as auth } from 'redux/ducks/auth';
 import initStore from 'redux/store';
+import request from 'utils/request';
 
 class ErrorPage extends Component {
-  static getInitialProps({ res, err }) {
+  static async getInitialProps(ctx) {
+    const { res, err } = ctx;
     const { statusCode = null } = res || err;
+    await request.populate(ctx, [auth.getCurrentUser]);
     return { statusCode };
   }
 
