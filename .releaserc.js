4ee833354ebbcf0b31cafc8de65eb37b1830c595
Ivan Pazhitnykh <ivan.pazhitnykh@gmail.com>
2018-10-14 05:27:01 +0300

    fix(tools): fix semantic `package.json` version update, add tgz publish

--- .releaserc.js
+++ .releaserc.js
@@ -16,12 +16,26 @@ module.exports = {
   prepare: [
     '@semantic-release/changelog',
     {
+      path: '@semantic-release/exec',
+      cmd: 'npm run prettier CHANGELOG.md',
+    },
+    {
+      path: '@semantic-release/npm',
+      npmPublish: false,
+      tarballDir: '.next',
+    },
+    {
       path: '@semantic-release/git',
       assets: ['package.json', 'package-lock.json', 'CHANGELOG.md'],
       message: '${nextRelease.version} release [skip ci]',
     },
   ],
-  publish: ['@semantic-release/github'],
+  publish: [
+    {
+      path: '@semantic-release/github',
+      assets: '.next/*.tgz',
+    },
+  ],
   success: ['@semantic-release/github'],
   fail: ['@semantic-release/github'],
 };
