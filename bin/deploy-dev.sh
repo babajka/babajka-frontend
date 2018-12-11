025c39fc9a9442b78ebdb6c96afb9e24889fda15
Uladzislau Bohdan <vlbohdan@gmail.com>
2018-12-11 12:13:08 +0300

    feat(tools): auto deployment of develop branch using Travis

---
+++ bin/deploy-dev.sh
@@ -0,0 +1,19 @@
+#!/usr/bin bash
+set -e
+
+DEPLOYMENTS_PATH="/home/wir-dev/deployed"
+
+rm -rf "${DEPLOYMENTS_PATH}/frontend"
+mv "${DEPLOYMENTS_PATH}/swap-frontend/babajka-frontend" "${DEPLOYMENTS_PATH}/frontend"
+
+cd "${DEPLOYMENTS_PATH}/frontend"
+
+# In order for the command below to work properly make sure that lines
+# case $- in
+#     *i*) ;;
+#       *) return;;
+# esac
+# in ~/.bashrc file on remote machine are commented out.
+#
+# Short explanation is: pm2 is not using interactive ssh.
+pm2 restart pm2-config.json --only=frontend-staging
