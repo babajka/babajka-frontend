348aaf987f4b82168c8612329d02edb94c575854
uladbohdan <vlbohdan@gmail.com>
2019-01-14 16:15:59 +0300

    chore(tools): deploy/install scripts are updated

--- bin/deploy-from-local.sh
+++ bin/deploy-from-local.sh
@@ -4,12 +4,12 @@ npm run build
 
 rsync -r --delete-after --exclude=.git --exclude=node_modules --exclude=styles . wir-dev@dev.wir.by:/home/wir-dev/deployed/swap-frontend/babajka-frontend/
 
-echo 'pushed on server'
+echo 'pushed to server'
 
 ssh wir-dev@dev.wir.by 'bash -s' < bin/install-prod-deps.sh
 
-echo 'dependencies installed'
+echo 'dependencies are installed'
 
-ssh wir-dev@dev.wir.by 'bash -s' < bin/deploy-dev.sh
+ssh wir-dev@dev.wir.by 'bash -s' < bin/postdeploy-dev.sh
 
 echo 'deployed'
