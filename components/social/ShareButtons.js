97ee5091cf1a5da845d25f59f18a6ac4daa75678
Victoria Drozd <imvictoriadrozd@gmail.com>
2019-07-31 01:56:01 +0300

    task(article): use external link for share buttons

--- components/social/ShareButtons.js
+++ components/social/ShareButtons.js
@@ -7,6 +7,7 @@ import qs from 'qs';
 import Text from 'components/common/Text';
 import Icon from 'components/common/ui/Icon';
 import ButtonGroup from 'components/common/ButtonGroup';
+import ExternalLink from 'components/common/ExternalLink';
 
 import { SHARE_NETWORKS } from 'constants/social';
 import { DOMAIN_SECURE } from 'constants';
@@ -17,16 +18,15 @@ const ShareButtons = ({ urlPath, title }) => (
       <Text
         id={`common.share-${id}`}
         render={t => (
-          <a
+          <ExternalLink
             key={id}
             className={`wir-button wir-button__icon wir-social-buttons__button wir-social-buttons__button--${id}`}
             href={`${baseUrl}?${qs.stringify(getParams(`${DOMAIN_SECURE}${urlPath}`, title))}`}
-            rel="noopener noreferrer"
-            target="_blank"
             title={t}
+            custom
           >
             <Icon pack="b" name={icon} />
-          </a>
+          </ExternalLink>
         )}
       />
     ))}
