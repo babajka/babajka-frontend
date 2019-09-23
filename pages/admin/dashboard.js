679c8cfc5a5cb64d4c95d95578b4b0d3216a274e
Ivan Pazhitnykh <ivan.pazhitnykh@gmail.com>
2019-09-23 16:37:38 +0300

    chore(core): remove stub admin pages [#70]

--- pages/admin/dashboard.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import withAdmin from 'components/hoc/withAdmin';
-
-const AdminDashboardPage = () => <div>TODO Admin Dashboard Page</div>;
-
-export default withAdmin(AdminDashboardPage);
