64196aea2667a25a6311016a9f95cbb30d2c6785
Ivan Pazhitnykh <ivan.pazhitnykh@gmail.com>
2019-09-29 22:30:07 +0300

    task(article): add article cover sources (responsive) [#74]

--- components/common/Image.js
+++ components/common/Image.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Image = ({ sources, alt, sizes }) => {
-  const srcSet = Object.entries(sources).reduce(
-    (acc, [size, link]) => `${acc}${link} ${size}w,`,
-    ''
-  );
-  return <img srcSet={srcSet} alt={alt} sizes={sizes} />;
+const Image = ({ className, sourceSizes, baseUrl, alt, sizes }) => {
+  const getSource = w => `${baseUrl}?w=${w} ${w}w,`;
+
+  const srcSet = sourceSizes.reduce((acc, w) => `${acc}${getSource(w)}${getSource(w * 2)}`, '');
+  return <img className={className} srcSet={srcSet} alt={alt} sizes={sizes} />;
 };
 
 Image.propTypes = {
-  sources: PropTypes.objectOf(PropTypes.string).isRequired,
+  className: PropTypes.string,
+  sourceSizes: PropTypes.arrayOf(PropTypes.number).isRequired,
+  baseUrl: PropTypes.string.isRequired,
   alt: PropTypes.string.isRequired,
   sizes: PropTypes.string,
 };
 
 Image.defaultProps = {
+  className: '',
   sizes: '',
 };
 
