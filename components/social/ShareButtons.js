a3354a85d31fd9f26f45bcc0c2b69dd9de62ab93
Ulad Bohdan <vlbohdan@gmail.com>
2020-01-28 17:44:42 +0300

    feat(social): share to telegram, popup window, diary text for twitter

--- components/social/ShareButtons.js
+++ components/social/ShareButtons.js
@@ -7,37 +7,51 @@ import qs from 'qs';
 import { localize, useLocaleContext } from 'components/common/Text';
 import Icon from 'components/common/ui/Icon';
 import ButtonGroup from 'components/common/ButtonGroup';
-import ExternalLink from 'components/common/ExternalLink';
 
 import { SHARE_NETWORKS } from 'constants/social';
 import { DOMAIN_SECURE } from 'constants';
 
-const ShareButtons = ({ urlPath, title }) => {
+const ShareButtons = ({ urlPath, text: { basic, extended } }) => {
   const lang = useLocaleContext();
   return (
     <ButtonGroup className="wir-social-buttons" icon>
-      {SHARE_NETWORKS.map(({ id, icon = id, baseUrl, getParams }) => (
-        <ExternalLink
-          key={id}
-          className={`wir-button wir-button__icon wir-social-buttons__button wir-social-buttons__button--${id}`}
-          href={`${baseUrl}?${qs.stringify(getParams(`${DOMAIN_SECURE}${urlPath}`, title))}`}
-          title={localize(`common.share-${id}`, lang)}
-          custom
-        >
-          <Icon pack="b" name={icon} />
-        </ExternalLink>
-      ))}
+      {SHARE_NETWORKS.map(({ id, icon = id, baseUrl, getParams }) => {
+        const text = id === 'twitter' ? extended || basic : basic;
+        return (
+          <button
+            key={id}
+            className={`wir-button wir-button__icon wir-social-buttons__button wir-social-buttons__button--${id}`}
+            title={localize(`common.share-${id}`, lang)}
+            type="button"
+            onClick={() => {
+              window.open(
+                `${baseUrl}?${qs.stringify(getParams(`${DOMAIN_SECURE}${urlPath}`, text))}`,
+                localize(`common.share-link`, lang),
+                'width=600,height=400'
+              );
+            }}
+          >
+            <Icon pack="b" name={icon} />
+          </button>
+        );
+      })}
     </ButtonGroup>
   );
 };
 
 ShareButtons.propTypes = {
   urlPath: PropTypes.string,
-  title: PropTypes.string.isRequired,
+  text: {
+    basic: PropTypes.string.isRequired,
+    extended: PropTypes.string,
+  },
 };
 
 ShareButtons.defaultProps = {
   urlPath: '',
+  text: {
+    extended: '',
+  },
 };
 
 export default ShareButtons;
