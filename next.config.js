8f40cc1cf3743cb8c7ebbccfea42e407d2900db8
Ivan Pazhitnykh <ivan.pazhitnykh@gmail.com>
2020-03-24 01:45:18 +0300

    fix(tools): next@9.3.1, apply localIdentName for prod [#101

--- next.config.js
+++ next.config.js
@@ -17,6 +17,30 @@ const { version } = packageJson;
 
 const nextConfig = {
   webpack(config) {
+    // HACK: https://github.com/zeit/next.js/issues/10339#issuecomment-596966509
+    const rule = config.module.rules.find(r => Object.keys(r).includes('oneOf'));
+    // Locate css-loader config for css modules
+    rule.oneOf.forEach(r => {
+      if (!Array.isArray(r.use)) {
+        return;
+      }
+      if (!'test.module.scss'.match(r.test) && !'test.module.css'.match(r.test)) {
+        return;
+      }
+      const cssLoader = r.use.find(u => u.loader.match('css-loader'));
+      if (cssLoader) {
+        cssLoader.options = {
+          ...cssLoader.options,
+          modules: {
+            ...cssLoader.options.modules,
+            // Fall back to default getLocalIdent function
+            getLocalIdent: () => false,
+            localIdentName: ENV === 'production' ? '[hash:base64:8]' : '[local]__[hash:base64:5]',
+          },
+        };
+      }
+    });
+
     config.plugins.push(
       ...[
         new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, new RegExp(langs)),
