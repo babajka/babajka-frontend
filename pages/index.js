825cdbd4017d0c67a3e72c03988cbc396ca0fa52
Ivan <ivan.pazhitnykh@gmail.com>
2020-12-27 17:31:08 +0300

    feat(diary): setup diary block ssr [#130 f-285]

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,20 @@
 import React, { useMemo } from 'react';
 
 import CardBlocksLayout from 'features/layout/card-blocks-layout';
+import { DiaryProvider } from 'features/diary/store';
+
 import { makeRequest } from 'utils/request';
 import { localizeData } from 'utils/getters';
 import { REVALIDATE_TIMEOUT } from 'constants/misc';
 import api from 'constants/api';
 
-const MainPage = ({ blocks, data, lang }) => {
+const MainPage = ({ blocks, data, diary, lang }) => {
   const localized = useMemo(() => localizeData(data, lang), [data, lang]);
-  return <CardBlocksLayout blocks={blocks} data={localized} />;
+  return (
+    <DiaryProvider value={diary}>
+      <CardBlocksLayout blocks={blocks} data={localized} />
+    </DiaryProvider>
+  );
 };
 
 MainPage.getLayoutProps = () => ({
@@ -18,8 +24,19 @@ MainPage.getLayoutProps = () => ({
 // TODO: use built-in i18n mechanism
 export const getStaticProps = async () => {
   const { blocks, data } = await makeRequest(api.storage.getMainPage);
+  const hasDiary = blocks.some(({ type }) => type === 'diary');
+
+  let diary;
+  if (hasDiary) {
+    diary = await makeRequest(api.diary.today);
+  }
+
   return {
-    props: { blocks, data },
+    props: {
+      blocks,
+      data,
+      diary,
+    },
     revalidate: REVALIDATE_TIMEOUT,
   };
 };
