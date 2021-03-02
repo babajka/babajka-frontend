d29cf9c940d1e13310ab9355acadaa1ef41a5f9d
Ulad Bohdan <vlbohdan@gmail.com>
2021-03-02 15:08:18 +0300

    feat(article): coub player styles for sizing

--- components/common/CoubPlayer.js
+++ components/common/CoubPlayer.js
@@ -1,27 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import bem from 'bem-css-modules';
 
-const CoubPlayer = ({ coubId, size }) => (
-  <span>
-    <iframe
-      title="CoubPlayer"
-      src={`//coub.com/embed/${coubId}?muted=false&autostart=false&originalSize=false&startWithHD=true`}
-      width={size}
-      height={size}
-      allow="autoplay"
-      frameBorder="0"
-      allowFullScreen
-    />
-  </span>
+import styles from './audioVideoPlayer.module.scss';
+
+const b = bem(styles);
+
+const CoubPlayer = ({ coubId }) => (
+  <div className={b()}>
+    <div className={b('coub')}>
+      <iframe
+        title="CoubPlayer"
+        src={`//coub.com/embed/${coubId}?muted=false&autostart=false&originalSize=false&startWithHD=true`}
+        width="100%"
+        height="100%"
+        allow="autoplay"
+        frameBorder="0"
+        allowFullScreen
+      />
+    </div>
+  </div>
 );
 
 CoubPlayer.propTypes = {
   coubId: PropTypes.string.isRequired,
-  size: PropTypes.string,
-};
-
-CoubPlayer.defaultProps = {
-  size: '480',
 };
 
 export default CoubPlayer;
