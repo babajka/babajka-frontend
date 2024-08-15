7a04cee0a68ad9d6b90fcb7c120ac69ffadaea59
Uladzislau Bohdan <vlbohdan@gmail.com>
2024-08-15 19:26:24 +0200

    cleanup about page, update twitter, enable suggested articles (#165)
    Co-authored-by: Ulad Bohdan <UladBohdan>

--- pages/about.js
+++ pages/about.js
@@ -5,7 +5,7 @@ import React from 'react';
 import cn from 'classnames';
 import bem from 'bem-css-modules';
 
-import ExternalLink from 'components/common/ExternalLink';
+// import ExternalLink from 'components/common/ExternalLink';
 import Text, { localize } from 'components/common/Text';
 import {
   MetaTitle,
@@ -19,7 +19,7 @@ import {
 // import { Thanks } from 'features/about/Thanks';
 // import team from 'data/team.json';
 
-import { PARTNERS } from 'constants/partners';
+// import { PARTNERS } from 'constants/partners';
 
 const b = bem(styles);
 // const t = bem(teammateStyles);
@@ -38,6 +38,7 @@ const AboutPage = ({ lang }) => (
       <div className={b('text', { 'margin-top': 'small' })}>
         <Text id="about.section1-text" />
       </div>
+      {/*
       <div
         className={cn(
           b('subheading', { 'mobile-only': true }),
@@ -46,7 +47,6 @@ const AboutPage = ({ lang }) => (
       >
         <Text id="about.section-team-header" />
       </div>
-      {/*
       <div className={b('team')}>
         <div className={cn(b('subheading'), t(), t('placeholder'))}>
           <Text id="about.section-team-header" />
@@ -66,7 +66,7 @@ const AboutPage = ({ lang }) => (
         </div>
       ))}
       */}
-      <div className={styles['about-page__text--margin-top--large']}>
+      {/* <div className={styles['about-page__text--margin-top--large']}>
         <div className={b('subheading')}>
           <Text id="about.section-partners-header" />
         </div>
@@ -79,7 +79,7 @@ const AboutPage = ({ lang }) => (
             </div>
           ))}
         </div>
-      </div>
+      </div> */}
     </div>
   </>
 );
